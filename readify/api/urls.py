4a034b3f1fc4781834ac7022ed8a7942b04dcf04
Евгения Богданова <ev002@list.ru>
2024-01-09 20:53:19 +0700

    some changes

--- readify/api/urls.py
+++ readify/api/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('', include(router_api_v1.urls))
-    # path('users/<int:pk>/book_read/', CustomUserViewSet.as_view({'get': 'get_book_read'}), name='user-book-read')
 ]
